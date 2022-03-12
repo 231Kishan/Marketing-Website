6d9f668c31c5a64b70c0833a0a0079c7dec8b8f7
231Kishan <101455702+231Kishan@users.noreply.github.com>
2022-03-12 17:30:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    
+     <React.Fragment>
+       <Header />
+       <Content />
+       <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
